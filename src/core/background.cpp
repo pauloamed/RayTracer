06d1356aa47da6b4c8ce2d5d753495661eb4b147
João Pedro <jpedrodeamorim@gmail.com>
2021-07-13 00:15:28 -0300

    finish background

--- src/core/background.cpp
+++ src/core/background.cpp
@@ -10,21 +10,22 @@ namespace rt3 {
  */
 
 Color interpolate_color(const float t, const Color &a, const Color &b){
-  return Color{
+  return Color{{
     Lerp(t, a.x(), b.x()),
     Lerp(t, a.y(), b.y()),
     Lerp(t, a.z(), b.z()),
-  };
+  }};
 }
 
 Color BackgroundColor::sampleXYZ(const Point2f &pixel_ndc) const {
-  float t_hor = pixel_ndc.x();
-  float t_ver = pixel_ndc.y();
+  float t_ver = pixel_ndc.x();
+  float t_hor = pixel_ndc.y();
   Color top = interpolate_color(t_hor, corners[Corners_e::tl], corners[Corners_e::tr]);
   Color bottom = interpolate_color(t_hor, corners[Corners_e::bl], corners[Corners_e::br]);
 
   return interpolate_color(t_ver, top, bottom);
 }
+
 
 BackgroundColor *create_color_background(const ParamSet &ps) {
   Color tl = retrieve( ps, "tl", Color());
@@ -32,10 +33,8 @@ BackgroundColor *create_color_background(const ParamSet &ps) {
   Color tr = retrieve( ps, "tr", Color());
   Color br = retrieve( ps, "br", Color());
 
-  auto mtype = retrieve( ps, "mapping", string("screen"));
-  
-  mapping_t mt = mapping_t::screen;
-  if(mtype == "spherical") mt = mapping_t::spherical;
-  return new BackgroundColor(mt, bl, tl, tr, br);
+  mapping_t mtype = retrieve( ps, "mapping", mapping_t::screen);
+
+  return new BackgroundColor(mtype, bl, tl, tr, br);
 }
 } // namespace rt3
