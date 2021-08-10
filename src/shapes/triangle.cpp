98003e6fa96c4d33110882df2264b62885722b40
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2021-08-10 14:11:57 -0300

    triangle ray intersection

--- src/shapes/triangle.cpp
+++ src/shapes/triangle.cpp
@@ -3,13 +3,8 @@
 
 namespace rt3{
 
-
-bool Triangle::intersect_p(const Ray &r, real_type maxT) const{
-  return true;
-}
-
-bool Triangle::intersect(const Ray &r, shared_ptr<ObjSurfel> &isect) const{
-  Vector3f edge[2] = {*v[1] - *v[0], *v[2] - *v[0]};
+bool Triangle::_intersect(const Ray &r, real_type &t, real_type &u, real_type &v) const{
+  Vector3f edge[2] = {*vert[1] - *vert[0], *vert[2] - *vert[0]};
 
 	Vector3f h = r.d.cross(edge[1]);
 	
@@ -17,23 +12,43 @@ bool Triangle::intersect(const Ray &r, shared_ptr<ObjSurfel> &isect) const{
 	if(abs(a) < EPS) return false; // This ray is parallel to this triangle.
 	
 	real_type f = 1 / a;
-	Vector3f s = r.o - *v[0];
+	Vector3f s = r.o - *vert[0];
 
-	real_type u = f * (s * h);
-	if(u < 0.0 || u > 1.0) return false; // ??
+	u = f * (s * h); // coordenada baricentrica u
+	if(u < 0.0 || u > 1.0) return false; 
 
 	Vector3f q = s.cross(edge[0]);
-	real_type v = f * (r.d * q);
-	if (v < 0.0 || u + v > 1.0) return false;
+	v = f * (r.d * q); // coordenada baricentrica v
+	if (v < 0.0 || u + v > 1.0) return false; 
 
-	real_type t = f * (edge[1] * q);
-	if (t > EPS){
-		// normal tem que ser uma media das normais
-		// 
-		// isect = unique_ptr<ObjSurfel>(new ObjSurfel(contact, normal, r.d * -1, t[0]));	
-		return true;
-	}else return false;
+	t = f * (edge[1] * q);
+	if (t < EPS) return false;
 
+	return true;
+}
+
+bool Triangle::intersect_p(const Ray &r, real_type maxT) const{
+	real_type t, u, v;
+	auto flag = _intersect(r, t, u, v);
+	if(!flag || t > maxT) return false;
+	else return true;
+}
+
+bool Triangle::intersect(const Ray &r, shared_ptr<ObjSurfel> &isect) const{
+	real_type t, u, v;
+	if(!_intersect(r, t, u, v)) return false;
+
+	Point3f contact = ((*vert[1] * u) + (*vert[2] * v) + (*vert[0] * (1 - u - v)));
+	// normal tem que ser uma media das normais
+	assert((contact - (r.o + r.d * t)).getNorm() < EPS);
+
+	Normal3f finalNormal = (*n[1] * u) + (*n[2] * v) + (*n[0] * (1 - u - v));
+	// cout << (*n[0]).toString() << " " << u << "\n";
+	// cout << (*n[1]).toString() << " " << v << "\n";
+	// cout << (*n[2]).toString() << " " << (1 - u - v) << "\n";
+
+	isect = unique_ptr<ObjSurfel>(new ObjSurfel(contact, finalNormal, r.d * -1, t));	
+	return true;
 }
 
 vector<Shape *> create_triangle_list( shared_ptr<TriangleMesh> mesh){
--- src/shapes/triangle.h
+++ src/shapes/triangle.h
@@ -9,34 +9,38 @@ namespace rt3{
 /// Represents a single triangle.
 class Triangle : public Shape {
 private:
-  shared_ptr<const Point3f> v[3];
+  shared_ptr<const Point3f> vert[3];
   shared_ptr<const Normal3f> n[3];
   shared_ptr<const Point2f> uv[3];
 
   shared_ptr<TriangleMesh> mesh; //!< This is the **indexed triangle mesh database** this triangle is linked to.
-  bool backface_cull;            //!< Set it `true` to activate backface culling.
 
 
 
 public:
   // The single constructor, that receives the mesh, this triangle id, and an indication for backface culling.
-  Triangle( shared_ptr<TriangleMesh> mesh, int tri_id, bool bfc=true )
-    : Shape(), mesh{mesh}, backface_cull{bfc}
+  Triangle( shared_ptr<TriangleMesh> mesh, int tri_id)
+    : Shape(), mesh{mesh}
   {
 
     auto &v_indexes = *(mesh->vertex_indices);
     auto &n_indexes = *(mesh->normal_indices);
-    auto &uv_indexes = *(mesh->uvcoord_indices);
-
+    
     auto &vertices = *(mesh->vertices);
     auto &normals = *(mesh->normals);
     auto &uvcoords = *(mesh->uvcoords);
 
     // This is just a shortcut to access this triangle's data stored in the mesh database.
     for(int i = 0; i < 3; ++i){
-      v[i] = vertices[v_indexes[ 3 * tri_id + i]];
+      vert[i] = vertices[v_indexes[ 3 * tri_id + i]];
       n[i] = normals[n_indexes[ 3 * tri_id + i]];
-      uv[i] = uvcoords[uv_indexes[ 3 * tri_id + i]];
+    }
+
+    if(mesh->uvcoord_indices){
+      auto &uv_indexes = *(mesh->uvcoord_indices);
+      for(int i = 0; i < 3; ++i){
+        uv[i] = uvcoords[uv_indexes[ 3 * tri_id + i]];
+      }
     }
   }
 
@@ -44,6 +48,8 @@ public:
   /// Return the triangle's bounding box.
   // Bounds3f object_bound() const;
   /// The regular intersection methods, as defined in the Shape parent class.
+  bool _intersect(const Ray &r, real_type &t, real_type &u, real_type &v) const;
+
   bool intersect_p(const Ray &r, real_type maxT) const override;
   bool intersect(const Ray &r, shared_ptr<ObjSurfel> &isect) const override;
 
