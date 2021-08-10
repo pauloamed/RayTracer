134a69c1b77e7723550c74a010e96ce41e62b3cc
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2021-08-10 15:25:35 -0300

    loading bar

--- src/core/integrator.cpp
+++ src/core/integrator.cpp
@@ -9,6 +9,14 @@ void SamplerIntegrator::render( const unique_ptr<Scene> &scene ) {
     // ...
     auto w = camera->film->width(); // Retrieve the image dimensions in pixels.
     auto h = camera->film->height();
+
+    int totIts = w * h;
+    int numberSteps = 50;
+    int its = 0;
+    cout << "[ ";
+
+  
+
     // Traverse all pixels to shoot rays from.
     for ( int i = 0 ; i < h; i++ ) {
         for( int j = 0 ; j < w ; j++ ) {
@@ -24,8 +32,14 @@ void SamplerIntegrator::render( const unique_ptr<Scene> &scene ) {
             Color pixelColor =  Li(ray, scene, backgroundColor);
          
             camera->film->add_sample( Point2i{{i,j}}, pixelColor ); // set image buffer at position (i,j), accordingly.
+
+            if(its % (totIts / numberSteps) == 0){
+                cout << "\b=>"; cout.flush();
+            }
+            its++;
         }
     }
+    cout << "]\n";
     // send image color buffer to the output file.
     camera->film->write_image();
 }
