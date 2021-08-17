fc59f0878764fee1b5791143ff4132b56e2f97e2
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2021-08-17 07:11:02 -0300

    fixed intersect code

--- src/core/bounds.cpp
+++ src/core/bounds.cpp
@@ -3,12 +3,12 @@
 namespace rt3{
 
 
-bool Bounds3f::intersect_p(const Ray &ray, pair<real_type, real_type> &hits) const{
+bool Bounds3f::box_intersect(const Ray &ray, pair<real_type, real_type> &hits) const{
   hits = {-INF, INF};
 
   for(int i = 0; i < 3; ++i){
     if(ray.d.at(i) == 0) continue;
-    real_type invDir = ray.d.at(i);
+    real_type invDir = 1.0 / ray.d.at(i);
     real_type minT = (minPoint.at(i) - ray.o.at(i)) * invDir;
     real_type maxT = (maxPoint.at(i) - ray.o.at(i)) * invDir;
     if(minT > maxT) swap(minT, maxT);
@@ -16,9 +16,22 @@ bool Bounds3f::intersect_p(const Ray &ray, pair<real_type, real_type> &hits) con
     hits.first = max(hits.first, minT);
     hits.second = min(hits.second, maxT);
   }
-
+  
   return hits.first < hits.second;
 }
+
+
+bool Bounds3f::intersect_p(const Ray &ray, real_type maxT) const{
+  pair<real_type, real_type> hits;
+  bool intersects = box_intersect(ray, hits);
+  if(!intersects) return false;
+  else{
+    if(hits.first > 0) return hits.first < maxT;
+    else if(hits.second > 0) return hits.second < maxT;
+    else return false;
+  }
+}
+
 
 Bounds3f Bounds3f::unite(const Bounds3f &a, const Bounds3f &b){
   Bounds3f x(a);
