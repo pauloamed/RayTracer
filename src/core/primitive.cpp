56e7163a269252823141b8d567ef44e8c6daf0f0
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2021-08-17 07:15:31 -0300

    moved primitives code to cpp file

--- src/core/primitive.cpp
+++ src/core/primitive.cpp
@@ -7,7 +7,36 @@ bool GeometricPrimitive::intersect_p( const Ray& r, real_type maxT ) const{
 }
 
 bool GeometricPrimitive::intersect(const Ray &r, shared_ptr<ObjSurfel> &isect ) const{
-    return shape->intersect(r, isect); 
+    if(shape->intersect(r, isect)){
+        auto selfRef = shared_ptr<const GeometricPrimitive>(shared_from_this());
+        isect->setPrimitive(selfRef);
+        return true;
+    }else return false; 
+}
+
+bool BVHAccel::intersect_p( const Ray& r, real_type maxT ) const{
+    if(boundingBox.intersect_p(r, maxT)){
+        for(auto &prim : primitives){
+            if(prim->intersect_p(r, maxT)) return true;
+        }
+        return false;
+    }else return false;
+}
+
+bool BVHAccel::intersect(const Ray &r, shared_ptr<ObjSurfel> &isect ) const{
+    pair<real_type, real_type> t;
+    if(!boundingBox.box_intersect(r, t)) return false;
+
+    shared_ptr<ObjSurfel> currIsect(nullptr);
+    for(auto &prim : primitives)
+    {   
+        if(prim->intersect(r, currIsect)){
+            if(isect == nullptr || currIsect->t < isect->t){
+                isect = currIsect;
+            }
+        }
+    }
+    return (isect != nullptr);
 }
 
 bool PrimList::intersect(const Ray &r, shared_ptr<ObjSurfel> &isect ) const{
@@ -31,5 +60,48 @@ bool PrimList::intersect_p(const Ray& r, real_type maxT) const{
     }
     return false;
 }
+
+vector<shared_ptr<BVHAccel>> BVHAccel::createLeaves(vector<shared_ptr<BoundedPrimitive>> &&prim, size_t leafSize){
+    vector<shared_ptr<BoundedPrimitive>> startList = std::move(prim);
+
+    vector<shared_ptr<BVHAccel>> leaves;
+    for(size_t i = 0; i < startList.size(); i += leafSize){
+
+        vector<shared_ptr<BoundedPrimitive>> currLeaf;
+
+        for(size_t j = i; j - i < leafSize; ++j){
+            currLeaf.push_back(startList[j]);
+        }
+
+        shared_ptr<BVHAccel> newNode{ new BVHAccel(std::move(currLeaf)) };
+        leaves.push_back(newNode);
+    }
+    
+    return leaves;
+}
+
+shared_ptr<BVHAccel> BVHAccel::build(vector<shared_ptr<BoundedPrimitive>> &&prim, size_t leafSize){
+
+    auto currList = createLeaves(std::move(prim), leafSize);
+
+    while(currList.size() != 1){
+        vector<shared_ptr<BVHAccel>> nextList;
+        for(size_t i = 0; i < currList.size(); i += 2){
+
+            shared_ptr<BVHAccel> newNode = nullptr;
+
+            if(i + 1 == currList.size()){
+                newNode = shared_ptr<BVHAccel>(new BVHAccel({currList[i]}));
+            }else{
+                newNode = shared_ptr<BVHAccel>(new BVHAccel({currList[i], currList[i + 1]}));
+            }
+
+            nextList.push_back(newNode);
+        }
+        currList = std::move(nextList);
+    }
+    return currList[0];
+}
+
 
 }
